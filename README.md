154b3ecb7c9e30648c4b6bb58882c998de21eedd
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2015-12-25 21:17:10 +0300

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+Basic Mocha+TypeScript sample
---
+++ lib/Calculator.ts
@@ -0,0 +1,9 @@
+export default class Calculator {
+  inc(x:number):number {
+    return x + 1;
+  }
+
+  add(x:number, y:number):number {
+    return x + y + 1;
+  }
+}
