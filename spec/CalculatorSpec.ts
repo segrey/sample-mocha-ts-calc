a2b59c72dccf0d9ca8b5b2896a48b17f33eb9dde
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2015-12-30 17:26:26 +0300

    add chai

--- spec/CalculatorSpec.ts
+++ spec/CalculatorSpec.ts
@@ -1,19 +1,24 @@
 /// <reference path="../typings/mocha/mocha.d.ts" />
+/// <reference path="../typings/chai/chai.d.ts" />
 import Calculator from '../lib/Calculator';
+import chai = require('chai');
+var expect = chai.expect;
 
 describe('Calculator', () => {
-    var subject:Calculator;
+  var calc:Calculator;
 
-    beforeEach(function () {
-        subject = new Calculator();
-    });
+  beforeEach(function () {
+    calc = new Calculator();
+  });
 
-    describe('#add', () => {
-        it('should add two numbers together', () => {
-            const result:number = subject.add(2, 3);
-            if (result !== 5) {
-                throw new Error('Expected 2 + 3 = 5 but was ' + result);
-            }
-        });
+  describe('#add', () => {
+    it('should add two numbers together', () => {
+      expect(calc.add(2, 2)).to.equals(4);
     });
+  });
+  describe('#inc', () => {
+    it('should increment number', () => {
+      expect(calc.inc(2)).to.equals(3);
+    });
+  });
 });
